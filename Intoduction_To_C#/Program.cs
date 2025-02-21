2dd4b4683b6ebc75449e30a0f28b8e70283e554b
georgeklangat <georgeklangat@gmail.com>
2025-02-21 20:15:02 +0300

    initial commit

---
+++ Intoduction_To_C#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+            Console.WriteLine("I am learning c#");
+            Console.WriteLine("Its awesome");
+            Console.WriteLine("i am a software developer");
+            Console.WriteLine(3 + 3);
+
+        }
+    }
+}
